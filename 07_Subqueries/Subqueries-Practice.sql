301fa1b81d9b5f18bf9e99ff07875161d271deeb
yadnyesh-96 <yadnyneshdhangar@gmail.com>
2026-03-11 17:19:37 +0530

    SQL- Subqueries -practices

---
+++ 07_Subqueries/Subqueries-Practice.sql
@@ -0,0 +1,22 @@
+
+
+CREATE TABLE department(
+deptId INT(5) PRIMARY KEY,
+deptName VARCHAR(200),
+location VARCHAR(200) 
+);
+
+CREATE TABLE employee1(
+empId INT(5) PRIMARY KEY, 
+empName VARCHAR(200),
+deptId INT(5), FOREIGN KEY (deptId) REFERENCES department(deptId),
+salary INT(5),
+join_year INT(5) 
+);
+
+CREATE TABLE project(
+pId INT(5) PRIMARY KEY,
+pName VARCHAR(200),
+empId INT(5), FOREIGN KEY (empId) REFERENCES employee(empId),
+budget INT(5)
+);
